075185919abb33247c094b36614f03ab1cccec2a
yichuanshuzi <chenboshuo.hi@163.com>
2019-02-04 09:07:24 +0800

    用clion可以成功运行.而且今天可以运行了

--- cpp_primer/chaper_1_getting_started/bookstore_problem.cpp
+++ cpp_primer/chaper_1_getting_started/bookstore_problem.cpp
@@ -2,10 +2,13 @@
 
 #include "Sales_item.h"
 int main(int argc, char const *argv[]) {
-  Sales_items boook;
+  Sales_item book;
   // read ISBN,number of copies sold,and sales princee
   std::cin >> book;
   // werit ISBN,number of cpoies sold,total revenue, and average princee
   std::cout << book << '\n';
   return 0;
 }
+// input
+// 123 5 10
+// 123 5 50 10
