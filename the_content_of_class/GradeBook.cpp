d6529d2ec3309333f44d6a4cf4e07111be7f8252
yichuanshuzi <chenboshuo.hi@163.com>
2019-02-26 21:03:22 +0800

    (习题3.11)修改Gradebook类 - 包括第二个string数据成员,他表示授课教师的名字 - 提供一个可以改变教师姓名的函数,以及可以得到名字的get - 修改构造函数,指定两个形参:一个针对课程名称, 另一个针对教师姓名. - 修改成员函数displaymessage,使它首先输出欢迎信息和课程名称,然后输出"This course is presented by:" 跟老师姓名

--- the_content_of_class/GradeBook.cpp
+++ the_content_of_class/GradeBook.cpp
@@ -10,8 +10,8 @@ using namespace std;
 class GradeBook {
 public:
   // construstor initialize courseName with string supplied as argument
-  explicit GradeBook(string name)
-    : courseName(name){// member initializer to initialize courseName
+  explicit GradeBook(string name, string teacher_name)
+  : courseName(name), teacher_name(teacher_name){// member initializer to initialize courseName
       // empty body
   }
 
@@ -27,27 +27,43 @@ public:
   }
 
   // funcxtion that displays a welcome message to the GradeBook user
+  // function that displays a welcome message to the GradeBook user
   void display_message(string courseName) const {
     cout << "Welcome to the Grade Book for\n" << courseName <<'\n';
   }
+  /**
+   * sets the teacher's name
+   * @param name
+   */
+  void set_teacher_name(string name) {
+    teacher_name = name;
+  }
 
+  string get_teacher_name(){
+    return teacher_name;
+  }
 private:
   string courseName; // course name for this GradeBook
+  string teacher_name;
 };
 
 // funstion main begins program execution
 int main(int argc, char const *argv[]) {
   // create two GradeBook objects
-  GradeBook gradeBook1("Introduction to C++ Programing");
-  GradeBook gradeBook2("Data strustures in C++");
+  GradeBook gradeBook1("Introduction to C++ Programing", "a");
+  GradeBook gradeBook2("Data strustures in C++", "b");
 
   // display initial value of courseName for each GradeBook
   cout << "gradeBook1 created for course: " << gradeBook1.get_course_name()
+    << "\n" << "This course is presented by: " << gradeBook1.get_teacher_name()
     << "\ngradeBook2 created for course: "<< gradeBook2.get_course_name()
-    << '\n';
+    << "\n" << "This course is presented by: " << gradeBook2.get_teacher_name()
+    << '\n' ;
 
 }
 /* output
 gradeBook1 created for course: Introduction to C++ Programing
+This course is presented by: a
 gradeBook2 created for course: Data strustures in C++
+This course is presented by: b
 */
