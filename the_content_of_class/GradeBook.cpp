11aa1758f7e0929c90642f3c2024f0bd0b5d5e82
chenboshuo.hi@163.com <chenbohsuo.hi@163.com>
2019-02-26 09:23:25 +0800

    3.3 传入参数

--- the_content_of_class/GradeBook.cpp
+++ the_content_of_class/GradeBook.cpp
@@ -3,22 +3,35 @@
  * create a GradeBook object,and call its displayMessage function
  */
 #include <iostream>
+#include <string>
 using namespace std;
 
 // GradeBook class definition
 class Gradebook {
 public:
   // funcxtion that displays a welcome message to the GradeBook user
-  void display_message() {
-    cout << "Welcome to the Grade Book" << '\n';
+  void display_message(string courseName) const {
+    cout << "Welcome to the Grade Book for\n" << courseName <<'\n';
   }
 };
 
 // funstion main begins program execution
 int main(int argc, char const *argv[]) {
+  string nameOfCourse;
   Gradebook myGradeBook;// create a GradeBook object named myGradeBook
-  myGradeBook.display_message(); // call object's displayMessage function
+
+  // prompt for and input course name
+  cout << "Place enter the course name " << '\n';
+  getline(cin, nameOfCourse);// read a course name with blanks
+  cout << '\n';
+
+  // call myGrade's displayMessage function
+  // and pass nameOfCourse as an argument
+  myGradeBook.display_message(nameOfCourse); // call object's displayMessage function
   return 0;
 }
+// input
+// cpp
 // output
-// Welcome to the Grade Book
+// Welcome to the Grade Book for
+// cpp
