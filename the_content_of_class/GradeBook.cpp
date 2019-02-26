64f1d875855d6d2f95e256e37f02ae8571a139b3
chenboshuo.hi@163.com <chenbohsuo.hi@163.com>
2019-02-26 09:43:50 +0800

    3.5 定义和测试私有类

--- the_content_of_class/GradeBook.cpp
+++ the_content_of_class/GradeBook.cpp
@@ -9,16 +9,32 @@ using namespace std;
 // GradeBook class definition
 class Gradebook {
 public:
+  // function that sets the course name
+  void set_course_name(string name ) {
+    courseName = name;
+  }
+
+  // function that gets the course name
+  string get_course_name() const{
+    return courseName;
+  }
+
   // funcxtion that displays a welcome message to the GradeBook user
   void display_message(string courseName) const {
     cout << "Welcome to the Grade Book for\n" << courseName <<'\n';
   }
+
+private:
+  string courseName; // course name for this GradeBook
 };
 
 // funstion main begins program execution
 int main(int argc, char const *argv[]) {
   string nameOfCourse;
   Gradebook myGradeBook;// create a GradeBook object named myGradeBook
+
+  // display initial value of CourseName
+  std::cout << "Initial course name is:" << myGradeBook.get_course_name() << '\n';
 
   // prompt for and input course name
   cout << "Place enter the course name " << '\n';
@@ -30,8 +46,13 @@ int main(int argc, char const *argv[]) {
   myGradeBook.display_message(nameOfCourse); // call object's displayMessage function
   return 0;
 }
-// input
-// cpp
-// output
-// Welcome to the Grade Book for
-// cpp
+/*
+input
+cpp
+
+output
+Initial course name is:
+// 说明私有对象不可访问
+Welcome to the Grade Book for
+cpp
+*/
