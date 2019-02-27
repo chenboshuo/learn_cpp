076118b5f251ab11c5e8f2e66abd9ab23d5b18a7
yichuanshuzi <chenboshuo.hi@163.com>
2019-02-27 20:53:29 +0800

    创建一个名为Account的类银行可使用它表示银行的账户. - [x]这个类应该包含一个数据成员表示账户余额. - [x]函数:接受余额并初始化数据成员(保证余额大于0,否则返回错误信息) - [x]credit:将一笔金额添加到余额中 - [x]debit:从这个类中取钱,并保证不能超出余额,若超出余额,指出"Debit amount exceed account balance" - [x]getBalance返回余额 - [x]测试程序,创建两个对象并测试函数

---
+++ homework/account.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class Account {
+private:
+  float balance;
+
+public:
+  /**
+   * 设置余额
+   * @param money 余额
+   */
+  void setBalance(float money) {
+    if (money > 0) {
+      balance = money;
+    } else {
+      std::cerr << "balance should be nonnegative" << '\n';
+    }
+  }
+  /**
+   * 存钱,保证金额大于0
+   * @param money 金额
+   */
+  void credit(float money) {
+    if (money > 0) {
+      balance += money;
+      std::cout << "the balance is " << balance << '\n';
+    } else {
+      std::cerr << "save money should be nonnegative" << '\n';
+    }
+  }
+
+  void debit(float money) {
+    if (money <= balance) {
+      balance -= money;
+      std::cout << "the balance is " << balance << '\n';
+    } else {
+      std::cout << "Debit amount exceed account balance" << '\n';
+    }
+  }
+
+  /**
+   * 返回余额
+   * @return 余额
+   */
+  float getBalance(){
+    return balance;
+  }
+
+};
+
+int main(int argc, char const *argv[]) {
+  Account my_alipay, my_wechat;
+  my_alipay.setBalance(0.00);
+  std::cout << "my_alipay:  " << my_alipay.getBalance() << '\n';
+  my_wechat.setBalance(5.00);
+  std::cout << "my_alipay:  " << my_wechat.getBalance() << '\n';
+  my_alipay.credit(3);
+  my_wechat.debit(4);
+  return 0;
+}
