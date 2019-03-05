65de11432fa8037e61a1371c95e5f004a0575931
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-05 19:09:10 +0800

    (图7.4)(按课件改)声明中初始化数组

--- the_content_of_class/array.cpp
+++ the_content_of_class/array.cpp
@@ -1,22 +1,17 @@
-// initializing an array
+// initializing an array in a declation
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int n[10];// n is an array of 10 integers
-
-  // initialize elements of array n to 0
-  for (size_t i = 0; i < 10; i++) {
-    n[i] = 0;
-  }
+  int n[10] = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
 
   cout << "Elements" << setw(13) << "value"<<'\n';
   /*
    * setw(int n)
    * 预设宽度,规定其后内容所占宽度
    * setfill(char c)在预设宽度如果存在没用完的宽度,用c填充
-   * 
+   *
    */
   // output each element's value
   for (size_t j = 0; j < 10; j++) {
@@ -26,14 +21,14 @@ int main(int argc, char const *argv[]) {
 }
 /*
 Elements        value
-      0            0
-      1            0
-      2            0
-      3            0
-      4            0
-      5            0
-      6            0
-      7            0
-      8            0
-      9            0
+      0           32
+      1           27
+      2           64
+      3           18
+      4           95
+      5           14
+      6           90
+      7           70
+      8           60
+      9           37
  */
