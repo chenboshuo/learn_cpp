58fd0f430f6e2e789898e2ea3546317b306a69e5
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-13 19:40:47 +0800

    上当了,出题人S* #include<iostream> using namespace std; //不允许改动程序其它地方。 //定义相关函数，实现三个整数相加，合理设置参数默认值，使得程序按要求正常输出。 int main() {     cout<<add(1,4)<<endl;     cout<<add(3)<<endl;     cout<<add(1,2,3)<<endl;     return 0; }

---
+++ homework/the-defule-of-parameters_2.cpp
@@ -0,0 +1,28 @@
+/**
+ * #include<iostream>
+using namespace std;
+//不允许改动程序其它地方。
+//定义相关函数，实现三个整数相加，合理设置参数默认值，使得程序按要求正常输出。
+int main()
+{
+    cout<<add(1,4)<<endl;
+    cout<<add(3)<<endl;
+    cout<<add(1,2,3)<<endl;
+    return 0;
+}
+ */
+
+#include<iostream>
+using namespace std;
+//不允许改动程序其它地方。
+//定义相关函数，实现三个整数相加，合理设置参数默认值，使得程序按要求正常输出。
+int add(int a,int b = 0, int c = 0){
+  return a+b+c;
+}
+int main()
+{
+    cout<<add(1,4)<<endl;
+    cout<<add(3)<<endl;
+    cout<<add(1,2,3)<<endl;
+    return 0;
+}
