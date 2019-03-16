e8884c43e65c374408f2bcaefa02fd82a199e105
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-16 08:45:16 +0800

    6.34 猜数游戏  * 编写一个程序,可以玩猜数字游戏.  * 程序在1-1000 之间的数随机选择被猜的数,然后显示  * I can have a number between 1 and 1000.  * Can you guess my number?  * Plase type your first guess  * 玩家于是输入猜想的一个数,程序会做出如下响应  * Excellent! You guessed the number!  * Would you like to try again?  (y or n)  * Too high. try again.  * Too low. try again.  * 如果玩家猜测是不正确的, 程序应继续循环,直到玩家猜对为止.  * 此过程需要一直提醒玩家是猜大了还是猜小了,从而让玩家尽快获得答案

---
+++ homework/guess_number.cpp
@@ -0,0 +1,61 @@
+/**
+ * 6.34 猜数游戏
+ * 编写一个程序,可以玩猜数字游戏.
+ * 程序在1-1000 之间的数随机选择被猜的数,然后显示
+ * I can have a number between 1 and 1000.
+ * Can you guess my number?
+ * Plase type your first guess
+ * 玩家于是输入猜想的一个数,程序会做出如下响应
+ * Excellent! You guessed the number!
+ * Would you like to try again?  (y or n)
+ * Too high. try again.
+ * Too low. try again.
+ * 如果玩家猜测是不正确的, 程序应继续循环,直到玩家猜对为止.
+ * 此过程需要一直提醒玩家是猜大了还是猜小了,从而让玩家尽快获得答案
+ */
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+/**
+ * 生成1-x的随机数
+ * @param  x 范围
+ * @return   随机数
+ */
+inline int random(int x){
+  srand((unsigned) time(NULL));
+  return rand() % x;
+}
+
+
+int main(int argc, char const *argv[]) {
+  while (1) {
+    int target = random(1000);
+    int guess = 1001; // 为了防止生成随机数直接与该变量相等
+
+    std::cout << "I can have a number between 1 and 1000.\n"
+    << "Can you guess my number?\n"
+    << "Plase type your first guess\n" << '\n';
+
+    while (target != guess) {
+      std::cin >> guess;
+      if(guess > target){
+        std::cout << "\n\tToo high. try again" << '\n';
+      }
+      if(guess < target){
+        std::cout << "\n\tToo low. try again" << '\n';
+      }
+    }
+    std::cout << "Excellent! You guessed the number!\n"
+      << "Would you like to try again?  (y or n) " << '\n';
+    // 判断结束
+    char sign = 0;
+    std::cin >> sign;
+    if (sign == 'n') {
+      break;
+    }
+    std::cout << "\n----------------------------------------" << '\n';
+  }
+  return 0;
+}
