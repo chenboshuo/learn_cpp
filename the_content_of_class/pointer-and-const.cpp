b5f3c4183c8c3ca46444571db7fac60a8eeff6b9
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-27 18:45:21 +0800

    (图8.11)试图通过修改指向非const的const指针

--- the_content_of_class/pointer-and-const.cpp
+++ the_content_of_class/pointer-and-const.cpp
@@ -1,20 +1,21 @@
 /**
- * 8.10
- * Attempting to modify data through a noconstant
- * pointer to constant data.
+ * 8.11
+ * Attempting to modify a constant pointer to nonconstant data
  */
 
-void f( const int *xPtr) {
-  *xPtr = 100// error cannot modified an illegal modification
-}
-
 int main(int argc, char const *argv[]) {
-  int y = 0;
-  f(&y);
-  return 0;
+  int x, y;
+
+  // ptr is a xonstant pointer to an integer that can
+  // be modified throu ptr ,  but ptr always points to
+  // the same memory location
+  int * const ptr = &x; // const pointer must be initialized
+
+  *ptr = 7;// allowed: *ptr is not xonst
+  ptr = &y; // error: ptr is const; cannot assign to it a new adress
 }
 /** output
- * pointer-and-const.cpp: In function 'void f(const int*)':
- * pointer-and-const.cpp:8:11: error: assignment of read-only location '* xPtr'
- *  *xPtr = 100;
+ * pointer-and-const.cpp: In function 'int main(int, const char**)':
+ * pointer-and-const.cpp:15:10: error: assignment of read-only variable 'ptr'
+ *    ptr = &y; // error: ptr is const; cannot assign to it a new adress
  */
