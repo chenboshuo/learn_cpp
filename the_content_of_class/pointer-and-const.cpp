db6baa2934abb76de6f1cde031dcf770127c1a10
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-27 18:50:40 +0800

    (图8.12)试图修改指向const数据的const指针

--- the_content_of_class/pointer-and-const.cpp
+++ the_content_of_class/pointer-and-const.cpp
@@ -1,21 +1,24 @@
 /**
- * 8.11
- * Attempting to modify a constant pointer to nonconstant data
+ * 8.12
+ * Attempting to modify a constant pointer to constant data
  */
 
 int main(int argc, char const *argv[]) {
-  int x, y;
+  int x = 5, y;
 
-  // ptr is a xonstant pointer to an integer that can
-  // be modified throu ptr ,  but ptr always points to
-  // the same memory location
-  int * const ptr = &x; // const pointer must be initialized
+  // ptr is a constant pointer to a const integer.
+  // ptr always points to the same location;
+  // the integer at that location cannot be modified
+  const int * const ptr = &x; // const pointer must be initialized
 
-  *ptr = 7;// allowed: *ptr is not xonst
+  *ptr = 7;// error: *ptr is const; cannot assigned assigned a new value
   ptr = &y; // error: ptr is const; cannot assign to it a new adress
 }
 /** output
  * pointer-and-const.cpp: In function 'int main(int, const char**)':
+ * pointer-and-const.cpp:14:10: error: assignment of read-only location '*(const int*)ptr'
+ * *ptr = 7;// error: *ptr is const; cannot assigned assigned a new value
+ *           ^
  * pointer-and-const.cpp:15:10: error: assignment of read-only variable 'ptr'
  *    ptr = &y; // error: ptr is const; cannot assign to it a new adress
  */
