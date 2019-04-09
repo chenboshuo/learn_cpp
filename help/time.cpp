45fc788c3606c8ea8abda68b8bbb1405db2184b2
chen boshuo <43953268+chenboshuo@users.noreply.github.com>
2019-04-09 08:55:39 +0800

    @JOANfcn 的一个错误

---
+++ help/time.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+
+#ifndef TIME_H
+
+#define TIME_H
+
+class Time {
+
+public:
+  explicit Time(int = 0, int = 0, int = 0);
+  void setTime(int, int, int);
+  void setHour(int);
+  void setMinute(int);
+  void setSecond(int);
+
+  unsigned int getHour() const;
+  unsigned int getMinute() const;
+  unsigned int getSecond() const;
+
+  void printStandard() const;
+  void tick(int,int,int) const;
+private:
+  unsigned int hour;
+unsigned int minute;
+unsigned int second;
+
+};
+#endif 
+
+
+// include "Time.h"
+
+using namespace std;
+
+Time::Time(int hour, int minute, int second){
+  setTime(hour, minute, second);
+  tick(hour, minute, second);
+}
+
+void Time::setTime(int h, int m, int s){
+  setHour(h);
+  setMinute(m);
+  setSecond(s);
+
+}
+
+void Time::setHour(int h){
+  if (h >= 0 && h < 24)
+    hour = h;
+  else
+    throw invalid_argument("hour must be 0-23");
+}
+
+void Time::setMinute(int m){
+  if (m >= 0 && m < 59)
+    minute = m;
+  else
+    throw invalid_argument("minute must be 0-59");
+}
+
+void Time::setSecond(int s){
+  if (s >= 0 && s < 59)
+    second = s;
+  else
+    throw std::invalid_argument("second must be 0-59");
+}
+
+unsigned int Time::getHour() const{
+  return hour;
+}
+
+unsigned int Time::getMinute() const{
+  return minute;
+}
+
+unsigned int Time::getSecond() const{
+  return second;
+}
+
+void Time::printStandard() const{
+  cout << ((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12) << ":" << setfill('0') << setw(2) << getMinute() << ":" << setw(2) << getSecond();
+}
+
+void Time::tick(int h,int m,int s) const{
+  if (s != 59)
+    s += 1;
+  else {
+    s = 0;
+    if (m != 59)
+      m += 1;
+    else{
+      m = 0;
+      h += 1;
+    }
+  }
+}
+
+
+using namespace std;
+
+int main() {
+  Time t(12, 54, 59);
+  t.printStandard();
+  try{
+    Time t2(99, 99, 99);
+  }catch (invalid_argument &e){
+    cerr << e.what();
+  }
+  system("pause");
+}
+/*
+报错信息
+terminate called after throwing an instance of 'std::invalid_argument'
+  what():  second must be 0-59
+ *、
