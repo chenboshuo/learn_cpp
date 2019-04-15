5230dd3a953ae48052cc8fe278330d8f5eaf6d21
yichuanshuzi <chenboshuo.hi@163.com>
2019-04-15 21:30:58 +0800

    9.10 const 对象和const函数 const对象不允许调用非const函数

--- the_content_of_class/Time.h
+++ the_content_of_class/Time.h
@@ -122,6 +122,34 @@ void Time::tick(int s) {
 }
 
 // poor practice : returning a reference to a private data number
+/*  test function:
+  Time t;
+
+  // initialize hourRef with the reference returned by bad_set_hour
+  unsigned int &hourRef = t.bad_set_hour(20); // 20 is a valid hour
+  cout << "Valid hour before modification: " << hourRef << endl;
+
+  hourRef = 30;
+  cout << "\nInvalid hour after modification: " << t.get_hour() << endl;
+
+  // Dangerous : Function call that returns
+  // a reference can be used as an Ivalue
+  t.bad_set_hour(12) = 74;
+  cout << "\nPOOR PROGRAMING PRACTICE!!!!!"
+    << "\nt.bad_set_hour(12) as an  Ivalue, invalid hour: "
+    << t.get_hour()
+    << endl;
+
+
+
+  output
+  alid hour before modification: 20
+
+Invalid hour after modification: 30
+
+POOR PROGRAMING PRACTICE!!!!!
+t.bad_set_hour(12) as an  Ivalue, invalid hour: 74
+*/
 unsigned int &Time::bad_set_hour(int hh){
   if (hh >= 0 && hh < 24) {
     hour = hh;
--- the_content_of_class/time.cpp
+++ the_content_of_class/time.cpp
@@ -1,35 +1,25 @@
 // filename: time.cpp
 #include "Time.h"
 int main(int argc, char const *argv[]) {
-  Time t;
-
-  // initialize hourRef with the reference returned by bad_set_hour
-  unsigned int &hourRef = t.bad_set_hour(20); // 20 is a valid hour
-  cout << "Valid hour before modification: " << hourRef << endl;
-
-  hourRef = 30;
-  cout << "\nInvalid hour after modification: " << t.get_hour() << endl;
-
-  // Dangerous : Function call that returns
-  // a reference can be used as an Ivalue
-  t.bad_set_hour(12) = 74;
-  cout << "\nPOOR PROGRAMING PRACTICE!!!!!"
-    << "\nt.bad_set_hour(12) as an  Ivalue, invalid hour: "
-    << t.get_hour()
-    << endl;
+  Time wake_up(6, 45, 0);// non-const object
+  const Time noon(12, 0, 0);// const object
+                          // onject      member function
+  wake_up.set_hour(18);   // non-const  non-const
+  noon.set_hour(12);      // const      non-const ---- error
+  wake_up.get_hour();     // non-const  const
+  noon.get_minute();      // const      const
+  noon.print_universal(); // const      const
+  noon.print_standard();  // const      non-const ---- error
 }
  /*
+time.cpp: In function 'int main(int, const char**)':
+time.cpp:8:19: error: passing 'const Time' as 'this' argument discards qualifiers [-fpermissive]
+   noon.set_hour(12);      // const      const
+                   ^
+In file included from time.cpp:2:
+Time.h:56:6: note:   in call to 'void Time::set_hour(int)'
+ void Time::set_hour(int h) {
+      ^~~~
 
-## 9.8 time实例研究--返回private数据成员的应用或指针
-
-对hourRef 的赋值其实在修改private的值, 破坏类的封装性
-output
-
-Valid hour before modification: 20
-
-Invalid hour after modification: 30
-
-POOR PROGRAMING PRACTICE!!!!!
-t.bad_set_hour(12) as an  Ivalue, invalid hour: 74
-
+toggle line 8, line 12 comments,Build success
  */
