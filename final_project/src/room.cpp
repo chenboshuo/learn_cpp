5a387c26d97a09765897c176fd3be4a0c9269193
yichuanshuzi <chenboshuo.hi@163.com>
2019-05-18 09:55:52 +0800

    room 类的初级实现

--- final_project/src/room.cpp
+++ final_project/src/room.cpp
@@ -1,1 +1,51 @@
-// filename: room.cpp 
+// filename: room.cpp
+// a test for room.h
+#include "room.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  Room room_1("101", "single room");
+  room_1.show();
+  room_1.check_in();
+  room_1.check_out();
+  /* output
+    room's room_number: 101
+    type: single room
+    The room is empty
+    The guests succeddfully checked in the room101
+    The gusest successfully checked out
+   */
+
+  cout << "\n" << endl;
+
+  Room room_2("102", "statand room", true);
+  room_2.show();
+  room_2.check_in();
+  room_2.check_out();
+  room_2.check_out();
+  /* room's number: 102
+    type: statand room
+    The room is full
+    The room is full, you can't chick into this room
+    Done
+    The room is empty, you can't do this
+   */
+
+  return 0;
+}
+/*
+room's room_number: 101
+type: single room
+The room is empty
+The guests succeddfully checked in the room101
+The gusest successfully checked out
+
+
+room's number: 102
+type: statand room
+The room is full
+The room is full, you can't chick into this room
+Done
+The room is empty, you can't do this
+ */
--- final_project/src/room.h
+++ final_project/src/room.h
@@ -1,1 +1,66 @@
 // filename: room.h
+#ifndef ROOM_H
+#define ROOM_H
+
+// #include "date.h"
+
+#include <string>
+#include <iostream>
+using namespace std;
+class Room {
+ public:
+   /**
+    * constructor 构造函数
+    * @param room_number    房间的编号(默认为" Unkonwn")
+    * @param room_type 房间类型(默认为""statand room"")
+    * @param status    状态(0代表空)
+    */
+  Room (string room_number = "Unkonwn",
+        string room_type = "statand room",
+        bool status = false)
+   :room_number(room_number), room_type(room_type), status(status){};
+
+  void show(void) {
+    cout << "room's room_number: "<< room_number << "\ntype: " << room_type
+      << "\nThe room is " << (status ? "full" : "empty")<< endl;
+  }
+
+  /**
+   * 入住酒店
+   * @return 1代表不能入住
+   */
+  int check_in(){
+    if(!status){
+      cout << "The guests succeddfully checked in the room " << room_number << endl;
+      status = true;
+      return 0;
+    }else{
+      cout << "The room is full, you can't chick into this room" << endl;
+      return 1;
+    }
+  }
+  /**
+   * 退房
+   * @return 1 代表非法操作
+   */
+  int check_out(){
+    if (status) {
+      cout << "The gusest successfully checked out" << endl;
+      status = false;
+      return 0;
+    } else {
+      cout << "The room is empty, you can't do this" << endl;
+      return 1;
+    }
+  }
+
+
+
+ private:
+  string room_number;
+  string room_type;
+  bool status;
+};
+
+
+#endif
