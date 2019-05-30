350596a5e70187f174e901cf3662a0a622af2211
yichuanshuzi <chenboshuo.hi@163.com>
2019-05-30 20:06:16 +0800

    guest 类的简单实现

--- final_project/src/guest.cpp
+++ final_project/src/guest.cpp
@@ -1,1 +1,18 @@
 // filename : guest.cpp
+#include <iostream>
+#include <string>
+
+#include "guest.h"
+#include "date.h"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  Date today(1,2,2018);
+  Guest a_man("100000", today,"Mr.a");
+  a_man.show();
+
+  cout << "\n\n";
+  Guest a_woman("108", today, "a", "girl");
+  a_woman.show();
+  return 0;
+}
--- final_project/src/guest.h
+++ final_project/src/guest.h
@@ -1,1 +1,55 @@
 // filename : guest.h
+#include <iostream>
+#include <algorithm>
+#include "date.h"
+
+using namespace std;
+#ifndef GUEST_H
+#define GUEST_H
+
+class Guest {
+ public:
+  /**
+   * 构造函数
+   * @param id_card       身份证
+   * @param chick_in_date 入住时间
+   * @param name          名称
+   * @param gender        性别(男性为0, 女性为1)
+   */
+  Guest (string id_card,
+         Date chick_in_date,
+         string name,
+         string gender = "man")
+   : id_card(id_card),
+   name(name){
+    chick_in_date = chick_in_date ;
+    if(gender == "famale" || "woman" || "girl"){
+      gender = 1;
+    }
+  };
+
+  /**
+   * 显示成员信息
+   */
+  void show() {
+    cout << (gender ? "Her" : "His") << " name is: "
+         << name << "\nID_card: " << id_card
+         << "\ncheck in date: " << chick_in_date << endl;
+  }
+
+  void check_in() {
+    cout << "Guset room check in" << endl;
+  }
+
+  void check_out() {
+    cout << "Guest room check out" << endl;
+  }
+
+ private:
+  string id_card;
+  Date chick_in_date;
+  string name;
+  bool gender = 0;  // 默认为男性
+};
+
+#endif
