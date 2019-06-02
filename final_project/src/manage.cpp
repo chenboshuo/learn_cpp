1fb986e5f33a1784772a75ec4eb901715df8eb7f
yichuanshuzi <chenboshuo.hi@163.com>
2019-06-02 15:43:41 +0800

    更新所有类的结构图

--- final_project/src/manage.cpp
+++ final_project/src/manage.cpp
@@ -9,10 +9,8 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   Manage m;
 
-  // // 测试客房信息的录入
-  // Room a("100", "single room", "fulle");
-  // m.new_room(a);
-  // m.show_room();
+  // 测试查找空房间
+  m.show_empty_room();
 
   // 录入客人信息
   Date today(1,10,2005);
@@ -20,14 +18,12 @@ int main(int argc, char const *argv[]) {
   m.check_in(a_woman, "107");
   m.show_guest();
 
-  // // 测试查找空房间
-  // m.show_empty_room();
-  //
-  // // 测试房间号查找;
-  // m.find_room("101");
-  // m.find_room("-1");
+  
+  // 测试房间号查找;
+  m.find_room("101");
+  m.find_room("-1");
 
-  // // 测试退房
-  // m.check_out("100");
+  // 测试退房
+  m.check_out("100");
   return 0;
 }
